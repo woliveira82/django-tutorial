5d14999c64e9c1c9c46dc2dd7e5e736a3700710c
Weiner Oliveira <weiner.oliveira@smartnx.com>
2020-03-22 11:35:19 -0300

    Add url from poll

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+from .models import Question
 # Create your views here.
+
+def index(request):
+    latest_question = Question.objects.order_by('pub_date')[:5]
+    output = ', '.join(q.question_text for q in latest_question)
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse(f'Detail view for {question_id}')
+
+def results(request, question_id):
+    return HttpResponse(f'Result view for {question_id}')
+    
+def vote(request, question_id):
+    return HttpResponse(f'Vote view for {question_id}')
